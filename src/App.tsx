28fdc802e7177d91609bcee5436f0312f8a67e6a
JakubPilkowski <pilkowskijakub@gmail.com>
2024-10-25 19:38:32 +0200

    Netlify deploy from github actions

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">
-          🚀 Rick and Morty - Fan Service
+          🚀 Rick and Morty - Fan Service With changes
           {/* {import.meta.env.VITE_TEST} {__TEAM__}{' '}
           {import.meta.env.VITE_XYZ_TEST_ENV}
           {import.meta.env.VITE_XYZ_INLINE} */}
