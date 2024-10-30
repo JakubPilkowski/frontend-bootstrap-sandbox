23b369763d0cb47e3d04002e0d5744f642f5a0f6
JakubPilkowski <pilkowskijakub@gmail.com>
2024-10-30 14:15:43 +0100

    chore: Update Terraform backend configuration for S3 bucket and DynamoDB table

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,13 +7,13 @@ terraform {
   }
 
   required_version = ">= 1.5.0"
-  # backend "s3" {
-  #   bucket         = "ofe-kalabanga-terraform-state-bucket"
-  #   key            = "state/terraform.tfstate"
-  #   region         = "eu-north-1"
-  #   encrypt        = true
-  #   dynamodb_table = "terraform-state-lock"
-  # }
+  backend "s3" {
+    bucket         = "ofe-kalabanga-terraform-state-bucket"
+    key            = "state/terraform.tfstate"
+    region         = "eu-north-1"
+    encrypt        = true
+    dynamodb_table = "terraform-state-lock"
+  }
 
 }
 
