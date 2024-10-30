e1a7a00ecd486e79ad6510581c107f0c61763c77
JakubPilkowski <pilkowskijakub@gmail.com>
2024-10-30 18:04:23 +0100

    chore: Update Sentry initialization and dependencies

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,9 +1,10 @@
 import * as Sentry from '@sentry/react';
 
 export function initSentry() {
-  if (import.meta.env.VITE_ENV_NAME !== 'production') {
+  if (import.meta.env.VITE_ENVIRONMENT !== 'Production') {
     return;
   }
+  console.log('Sentry initialized');
   Sentry.init({
     dsn: import.meta.env.VITE_SENTRY_DSN,
     integrations: [],
