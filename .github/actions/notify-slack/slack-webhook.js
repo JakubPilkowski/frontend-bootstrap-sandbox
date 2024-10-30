65309510b0c15f7887d99e1157310d4606a9aecb
JakubPilkowski <pilkowskijakub@gmail.com>
2024-10-30 18:21:50 +0100

    chore: Update notify-slack action to use axios for HTTP requests

---
+++ .github/actions/notify-slack/slack-webhook.js
@@ -0,0 +1,35 @@
+import { axios } from 'axios';
+
+const { SLACK_WEBHOOK_URL, NOTIFY_MESSAGE, NOTIFY_TYPE } = process.env;
+
+if (!SLACK_WEBHOOK_URL) {
+  throw new Error('SLACK_WEBHOOK_URL is required');
+}
+
+if (!NOTIFY_MESSAGE) {
+  throw new Error('NOTIFY_MESSAGE is required');
+}
+
+const requestConfig = {
+  url: SLACK_WEBHOOK_URL,
+  message: NOTIFY_MESSAGE,
+  type: NOTIFY_TYPE || 'good',
+};
+
+async function notifySlack(config) {
+  try {
+    await axios.post(config.url, {
+      text: 'GitHub Actions',
+      attachments: [
+        {
+          color: config.type,
+          text: config.message,
+        },
+      ],
+    });
+  } catch (error) {
+    console.error('Error sending message to Slack', error);
+  }
+}
+
+await notifySlack(requestConfig);
