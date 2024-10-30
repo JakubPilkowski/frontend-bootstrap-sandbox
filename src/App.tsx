d25408c343c62349ac0cf4dba20a03b81dc89076
JakubPilkowski <pilkowskijakub@gmail.com>
2024-10-30 11:14:33 +0100

    chore: Configure AWS credentials for deployment to eu-north-1

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
     <div>
       <div className="bg-white rounded-lg shadow-sm p-4 mb-4">
         <h1 className="text-xl font-bold text-center">
-          🚀 Rick and Morty - Fan Service With changes
+          🚀 Rick and Morty - Hello from aws
           {/* {import.meta.env.VITE_TEST} {__TEAM__}{' '}
           {import.meta.env.VITE_XYZ_TEST_ENV}
           {import.meta.env.VITE_XYZ_INLINE} */}
